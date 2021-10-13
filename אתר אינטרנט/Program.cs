e52cda22706f1bc8b627126a73ee5415b48dee6f
roniii12 <49608859+roniii12@users.noreply.github.com>
2021-10-13 23:35:54 +0300

    Add project files.

---
+++ אתר אינטרנט/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace אתר_אינטרנט
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
